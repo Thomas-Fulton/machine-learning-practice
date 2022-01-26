0da5c81f0a4ff0ffa6e6b2e85a1c93bf7279f086
Thomas-Fulton <thomas@fultondesigns.co.uk>
2022-01-26 00:10:41 +0000

    rough idea: automate checking if data meets assumptions of different estimators

---
+++ check_assumptions.py
@@ -0,0 +1,14 @@
+
+
+
+class CheckAssumptions:
+    def __init__:
+        self.meets_all = False
+        self.summary = []
+        self.assumptions = []
+        
+
+class CheckLogisticReg(CheckAssumptions):
+    def __init__:
+        __super__.__init__()
+
