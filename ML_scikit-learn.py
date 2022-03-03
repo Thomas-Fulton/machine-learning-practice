03c96f9d1daad6c5ae7bb1181b011ef39859a5d2
thomas-fulton <thomas@fultondesigns.co.uk>
2022-03-03 16:59:15 +0000

    exploratory plots and pandas

--- ML_scikit-learn.py
+++ ML_scikit-learn.py
@@ -28,8 +28,19 @@ Random Forest
 
 '''
 
-import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from pandas.plotting import parallel_coordinates
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn import metrics
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
 
 from sklearn import datasets
 from sklearn.preprocessing import StandardScaler
@@ -37,6 +48,9 @@ from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
 from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import train_test_split, KFold, StratifiedKFold, cross_val_score
 
+    #### Load data ####
+    ## Numpy ##
+# used in first attempt when building model
 iris = datasets.load_iris()
 #print(iris.DESCR)
 #print(dir(iris))
@@ -47,16 +61,74 @@ iris = datasets.load_iris()
 #print(iris.data.ndim)
 print(iris.data)
 
-# Plot
-# Check assumptions
+    ## Pandas ##
+pddata = pd.read_csv("https://raw.githubusercontent.com/terryz1/Iris_Classification/master/data/data.csv")  # use raw link from github
+pddata.head()
+
+    ## Plot and explore data ##
+pddata.describe()
+# see distribution of samples in each category TODO if uneven distribution?
+pddata.groupby('species').size()
+
+#plt.interactive(True)
+
+## Histograms
+# see distribution for each feature
+#plt.ioff()
+n_bins = 10
+plt.scatter(pddata['sepal_length'], pddata['sepal_width'])
+#plt.hist(pddata['sepal_length'], bins = n_bins)
+plt.show()
+#hist00.set_title('Sepal Length')
+#hist01 = plt.hist(pddata['sepal_width'], bins = n_bins)
+#hist01.set_title('Sepal Width')
+#hist10 = plt.hist(pddata['petal_length'], bins = n_bins)
+#hist10.set_title('Petal Length')
+#hist11 = plt.hist(pddata['petal_width'], bins = n_bins)
+#hist11.set_title('Petal Width')
+# add some spacing between subplots
+#fig.tight_layout(pad=1.0)
+#hist00.show()
+
+# Boxplots
+fig, axs = plt.subplots(2, 2)
+fn = ["sepal_length", "sepal_width", "petal_length", "petal_width"]
+cn = ['setosa', 'versicolor', 'virginica']
+sns.boxplot(x = 'species', y = 'sepal_length', data = pddata, order = cn, ax = axs[0,0]);
+sns.boxplot(x = 'species', y = 'sepal_width', data = pddata, order = cn, ax = axs[0,1]);
+sns.boxplot(x = 'species', y = 'petal_length', data = pddata, order = cn, ax = axs[1,0]);
+sns.boxplot(x = 'species', y = 'petal_width', data = pddata,  order = cn, ax = axs[1,1]);
+# add some spacing between subplots
+fig.tight_layout(pad=1.0);
+plt.show(block=True)
+
+# Violin plots
+sns.violinplot(x="species", y="petal_length", data=pddata, size=5, order = cn, palette = 'colorblind');
+plt.show(block=True)
+
+# Paired scatterplots
+pairplot = sns.pairplot(pddata, hue="species", height = 2, palette = 'colorblind');
+plt.show(block=True)
+
+# Correlation between variables heatmap
+corrmat = pddata.corr()
+sns.heatmap(corrmat, annot = True, square = True);
+plt.show(block=True)
+
+# alternative
+parallel_coordinates(pddata, "species", color = ['blue', 'red', 'green']);
+plt.show(block=True)
+
+    ## Check assumptions ##
 
 
-## Scaling (linear) ##
+    ## Scaling (linear) ##
+
 # (If your data has lots of outliers a more robust scaler should be used)
 # First fit the scaler to the data
 scaler = StandardScaler().fit(iris.data)
 scaler.mean_ # shows the mean of the data
-print("\n\nScale factor: ", scaler.scale_) # shows the scale factor of the normalised data
+print("\n\nScale factor: ", scaler.scale_)  # shows the scale factor of the normalised data
 
 # Then scale the data itself
 iris_scaled = scaler.transform(iris.data)
@@ -64,8 +136,8 @@ type(iris_scaled)
 
 logreg = LogisticRegressionCV(max_iter=1000)
 
-# Cross validation: return
-# Use stratifiedKFold if the estimator is a classifier, and target is binary or multiclass (keeps distribution of categories in target class the same in each subset)
+# Cross validation: return Use stratifiedKFold if the estimator is a classifier, and target is binary or multiclass (
+# keeps distribution of categories in target class the same in each subset)
 k_fold = StratifiedKFold(n_splits=10, shuffle=True, random_state=666)
 res = cross_val_score(logreg, iris_scaled, iris.target, cv=k_fold.split(iris_scaled, y=iris.target), n_jobs=-1)
 print("Accuracy: %.3f%% (%.3f%%)" % (res.mean()*100.0, res.std()*100.0))
--- README.md
+++ README.md
@@ -8,11 +8,12 @@ Revision and development of statistical/machine learning knowledge and skills. C
 5. To learn how to compare different models' performance and predictions
 6. To create a basic framework with scikit-learn so complete pipelines with more than one model can be easily implemented (with cross-validation and no training/testing data leakage).  
 
-####TODO:
+###TODO:
 - Bayesian statistical modelling methods
-- Basic neural network with pima dataset
-- check assumptions functions
 - parameter inference
+- time series
+- Basic neural network with pima dataset
+- check_assumptions functions
 - Finish steps and considerations below
 
 ## Steps and considerations when solving a problem through statistical/machine learning
@@ -35,11 +36,12 @@ What do you want to predict? Do you want to see the effect of certain factors, o
 What information can be used as features, and what variable do you want to predict?
 
 ## 2: Analyse data
-Look at the raw data: the number of rows and columns `data.head(20)` `data.shape`; which columns are features and which are labels `data.names`; is there any missing data
+Look at the raw data: the number of rows and columns `data.head(20)` `data.shape`; which columns are features and which are labels `data.names`; is there any missing data? Balanced class distributions? (TODO unbalanced)
 
 ## 3:
 
 
+### Links
 ideas for datasets:
 - https://www.dataquest.io/blog/free-datasets-for-projects/ 
 - papers
@@ -49,6 +51,7 @@ Introduction to machine learning
 - Mini-course: https://machinelearningmastery.com/python-machine-learning-mini-course/
 - Pipelines: https://machinelearningmastery.com/machine-learning-modeling-pipelines/
 - Quick regressions: https://machinelearningmastery.com/spot-check-regression-machine-learning-algorithms-python-scikit-learn/
+- https://towardsdatascience.com/exploring-classifiers-with-python-scikit-learn-iris-dataset-2bcb490d2e1b
 
 Choosing an algorithm tips:
 - https://www.kdnuggets.com/2020/05/guide-choose-right-machine-learning-algorithm.html
